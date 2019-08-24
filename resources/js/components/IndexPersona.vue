ba51133900126d20ad77a606abc1b61f7546b145
Roberto <roberto@morales.com.co>
2019-08-24 09:53:10 -0500

    Bonton Agregar y Fuse agregados

--- resources/js/components/IndexPersona.vue
+++ resources/js/components/IndexPersona.vue
@@ -2,19 +2,28 @@
   <div class="page-container">
     <md-app md-mode="fixed">
       <md-app-toolbar class="md-primary">
-        <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
-          <md-icon>menu</md-icon>
-        </md-button>
-        <span class="md-title">My Title</span>
-        
+        <div class="md-toolbar-row">
+          <div class="md-toolbar-section-start">
+              <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
+                  <md-icon>menu</md-icon>
+              </md-button>
+              <span class="md-title">My Title</span>
+          </div>
+         <vue-fuse 
+         :keys="keys" 
+         :list="bikes" 
+         :defaultAll="false" 
+         :eventName="bikesChanged"
+         class="fuse">
+         </vue-fuse>
         <div class="md-toolbar-section-end">
           <md-button class="md-icon-button">
             <md-icon>refresh</md-icon>
           </md-button>
-
           <md-button class="md-icon-button">
             <md-icon>more_vert</md-icon>
           </md-button>
+        </div>
         </div>
       </md-app-toolbar>
 
@@ -36,10 +45,15 @@
           
         </md-list>
       </md-app-drawer>
-
       <md-app-content>
-      <card-persona></card-persona>  
+        <card-persona></card-persona> 
+      
+        <md-button class="md-fab md-primary md-fab-bottom-right">
+          <md-icon>add</md-icon>
+        </md-button> 
       </md-app-content>
+      
+      
     </md-app>
   </div>
 </template>
@@ -50,10 +64,14 @@
     border: 1px solid rgba(#000, .12);
   }
 
-   // Demo purposes only
   .md-drawer {
     width: 230px;
     max-width: calc(100vw - 125px);
+  }
+
+  .fuse{
+    width:100%;
+    margin-left:5px;
   }
 </style>
 
